de7ee145032404769f10def495d3376a6cd38cf7
Hendricks Liu <hendricks.liu@oocl.com>
2019-07-12 14:01:25 +0800

    [Hendricks][feat:implement when student is 1 & test:should return 2 when student is 2]

--- src/main/java/com/thoughtworks/tdd/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzz.java
@@ -1,4 +1,7 @@
 package com.thoughtworks.tdd;
 
 public class FizzBuzz {
+    public String say(int student) {
+        return "1";
+    }
 }
--- src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/tdd/FizzBuzzTest.java
@@ -10,11 +10,22 @@ public class FizzBuzzTest {
     @Test
     public void should_return_1_when_student_is_1() {
         // given
-        int student =1;
+        int student = 1;
         FizzBuzz fizzBuzz = new FizzBuzz();
         // when
         String result = fizzBuzz.say(student);
         // then
         assertThat(result, CoreMatchers.is("1"));
+    }
+
+    @Test
+    public void should_return_2_when_student_is_2() {
+        // given
+        int student = 2;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        // when
+        String result = fizzBuzz.say(student);
+        // then
+        assertThat(result, CoreMatchers.is("2"));
     }
 }
